c71a99d55e1d16e0a35df80de9c66b99dab8cfda
iipanda <iipandarx@protonmail.com>
2024-05-22 19:02:55 +0200

    Add more tests

--- src/paynow.spec.ts
+++ src/paynow.spec.ts
@@ -16,4 +16,18 @@ describe("Paynow", () => {
     expect(paynow.payments).toBeDefined();
     expect(paynow.payments).toBeInstanceOf(Payments);
   });
+
+  it("uses the correct base url", () => {
+    expect(Reflect.get(paynow, "API_URL")).toBe("https://api.paynow.pl/");
+
+    const sandbox = new Paynow({
+      apiKey: "test",
+      secret: "test",
+      isSandbox: true,
+    });
+
+    expect(Reflect.get(sandbox, "API_URL")).toBe(
+      "https://api.sandbox.paynow.pl/",
+    );
+  });
 });
